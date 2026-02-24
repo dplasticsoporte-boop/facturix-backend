e1df047666d4d238f6405916a71e45f68caea7ae
dplasticsoporte <dplastic.soporte@gmail.com>
2026-02-24 01:09:46 -0500

    Configura Firebase con variables de entorno

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,20 @@
 import admin from "firebase-admin";
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT no definida");
+}
+
+if (!process.env.FIREBASE_DATABASE_URL) {
+  throw new Error("FIREBASE_DATABASE_URL no definida");
+}
+
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DB_URL
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 export const db = admin.database();
